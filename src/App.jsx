d7eb24133f9e4a2ec1f8c31f1d8994f5b1385865
Marko_Askovic <marko.askovic@casasoft.com>
2023-08-24 18:00:17 +0200

    added custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import JournalList from './components/JournalList/JournalList';
 import Body from './layouts/body/Body';
 import LeftPanel from './layouts/leftPanel/leftPanel';
 import JournalForm from './components/JournalForm/JournalForm';
+import { useLocalStorage } from './hooks/use-localstorage.hook';
 
 function App() {
 	const INITIAL_DATA = [
@@ -31,42 +32,58 @@ function App() {
 		tag: ''
 	};
 
-	const [items, setItems] = useState(INITIAL_DATA);
+	//	const [items, setItems] = useState(INITIAL_DATA);
+	const [items, setItems] = useLocalStorage('data');
+
 	const [selectedItem, setSelectedItem] = useState(INITIAL_SELECTED_ITEM);
 
-	useEffect(() => {
+	/* 	useEffect(() => {
 		const data = JSON.parse(localStorage.getItem('data'));
 		if (data) {
-			console.log('get items from localStorage'); // fait 2 fois parce que régime strict en mode dev
-			setItems(
+			console.log('get items from localStorage'); */ // fait 2 fois parce que régime strict en mode dev
+	/* 			setItems(
 				data.map((item) => ({
 					...item,
 					date: new Date(item.date)
 				}))
 			);
 		}
-	}, []);
+	}, []); */
 
-	useEffect(
-		/* () => console.log(items) */
-		() => {
+	/* useEffect( */
+	/* () => console.log(items) */
+	/* 		() => {
 			if (items.length) {
 				console.log('enregistrement');
 				localStorage.setItem('data', JSON.stringify(items));
 			}
 		},
 		[items]
-	);
+	); */
 
 	// API call => items are updated, state is updated, so another render occurs
 	// then another API call => items are again updated, state is updated, so another render occurs
 	// and so on - infinite loop occurs
 	// to prevent this, useEffect hook is necessary
 
+	function mapItems(items) {
+		if (!items) {
+			return [];
+		}
+		return items.map(i => ({
+			...i,
+			date: new Date(i.date)
+		}));
+	}
+
 	const addItem = (item) => {
-		console.log(item);
 		if (!item.id) {
-			setItems((oldItems) => [
+			setItems([...mapItems(items), {
+				...item,
+				date: new Date(item.date),
+				id: items.length > 0 ? Math.max(...items.map(i => i.id)) + 1 : 1
+			}]);
+			/* 			setItems((oldItems) => [
 				...oldItems,
 				{
 					...item,
@@ -76,20 +93,29 @@ function App() {
 						? Math.max(...oldItems.map((item) => item.id)) + 1
 						: 1
 				}
-			]); 
+			]);  */
 		} else 
 		{
-			setItems((oldItems) => [
-				...oldItems.map(i => {
-					if (i.id === item.id) {
-						return {
-							...item,
-							date: new Date(item.date)
-						};
-					}
-					return i;
-				})
-			]);
+			setItems([...mapItems(items).map(i => {
+				if (i.id === item.id) {
+					return {
+						...item
+					};
+				}
+				return i;
+			})]);
+		
+			/* 		setItems((oldItems) => [
+			...oldItems.map(i => {
+				if (i.id === item.id) {
+					return {
+						...item,
+						date: new Date(item.date)
+					};
+				}
+				return i;
+			})
+		]); */
 		}
 	//	setSelectedItem(INITIAL_SELECTED_ITEM); // clear form
 	};
@@ -112,7 +138,7 @@ function App() {
 			<LeftPanel>
 				<Header />
 				<JournalAddButton clearForm={() => setSelectedItem(INITIAL_SELECTED_ITEM)} />
-				<JournalList items={items} setItem={setSelectedItem} />
+				<JournalList /* items={items} */ items={mapItems(items)} setItem={setSelectedItem} />
 				{/* 	{[<Button></Button>, <Button></Button>]} */}
 				{/* 		{items.length === 0 && (
 						<p>Pas de notes pour le moment, veuillez rajouter la première</p>
