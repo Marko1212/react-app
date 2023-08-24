1ae036ee9c6167fe069f8ea45bf636a2e97641d9
Marko_Askovic <marko.askovic@casasoft.com>
2023-08-24 15:02:16 +0200

    slight change

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './JournalForm.module.css';
 import Button from '../Button/Button';
 import cn from 'classnames';
